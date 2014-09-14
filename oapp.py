c59061e04565348dfc79bd77541f3d6c4f96aa3c
Eric Jones <eric@ercjns.com>
2014-09-14 14:18:53 -0700

    sub of conversion to flask-sqlalchemy

--- oapp.py
+++ oapp.py
@@ -7,79 +7,87 @@
 ################################################
 
 
-# set-up commands:
-# set an environment variable to load configs from:
-# export OAPP_TEST_SETTINGS='/home/eric/code/oapp/config.ini'
-# start the viretual envrionment:
-# . venv/bin/activate
-# open a python prompt and init the db:
-# >>> from oapp import init_db, update_course_db()
-# >>> init_db()
-# >>> update_course_db()
-# quit that python prompt, run the app from the console:
-# python oapp.py
+#Switching to PostgreSQL for deployment on heroku dev.
 
 
 # imports
-import sqlite3
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
 from contextlib import closing
 from flask import Flask, render_template, url_for, redirect, g, session
 
-# Create the Flask application
-app =  Flask(__name__)
-app.config.from_envvar('OAPP_TEST_SETTINGS', silent=True)
 
+################################################
+# Create the Flask application, connect to the db
+################################################
+
+app =  Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URL'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 
 ################################################
 # Set up the databases, define database access functions
 ################################################
 
-def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+class Runner(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	venuecode = db.Column(db.String(10))
+	course = db.Column(db.Integer)
+	punch = db.Column(db.Integer)
+	punch_on_course = db.Column(db.Integer)
+	punch_time = db.Column(db.String(100))
+	start_time = db.Column(db.String(100))
+	end_time = db.Column(db.String(100))
+	finished = db.Column(db.Integer)
 
-def init_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+	def __init__(self, venuecode, course):
+		self.venuecode = venuecode
+		self.course = course
 
-def update_course_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('courses.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+	def __repr__(self):
+		return '<Runner at %s on course %d>' % self.venuecode, self.course
 
-@app.before_request
-def before_request():
-	g.db = connect_db()
+class Course(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	venuecode = db.Column(db.String(10))
+	venuefull = db.Column(db.String(100))
+	coursecode = db.Column(db.Integer)
+	coursefull = db.Column(db.String(100))
+	distance = db.Column(db.String(20))
+	climb = db.Column(db.String(20))
+	start = db.Column(db.Integer)
+	controls = db.Column(db.String(100))
+	finish = db.Columb(db.Integer)
 
-@app.teardown_request
-def teardown_request(exception):
-	db = getattr(g, 'db', None)
-	if db is not None:
-		db.close()
+	def __init__(self, venuecode, venuefull, coursecode, coursefull,
+				distance, climb, start, controls, finish):
+		self.venuecode = venuecode
+		self.venuefull = venuefull
+		self.coursecode = cousecode
+		self.coursefull = coursefull
+		self.distance = distance
+		self.climb = climb
+		self.start = start
+		self.controls = controls
+		self.finish = finish
 
-def make_dicts(cursor, row):
-	return dict((cursor.description[idx][0], value)
-				for idx, value in enumerate(row))
+	def __repr__(self):
+		return '<course %i at %s>' % self.coursecode, self.venuefull
 
-def get_db():
-	db = getattr(g, 'db', None)
-	if db is None:
-		db = g.db = connect_db()
-	db.row_factory = make_dicts
-	return db
 
-def query_db(query, args=(), one=False):
-	cur = get_db().execute(query, args)
-	if one:
-		rv = cur.fetchone()
-	else:
-		rv = cur.fetchall()
-	cur.close()
-	return rv
-
+def _fillCourseTable():
+	mag1 = Course('mag', 'Magnuson Park', 1, 'Beginner', '1.4 km', '0m', 0, '0,31,32,33,34,35,36,999', 999)
+	mag2 = Course('mag', 'Magnuson Park', 2, 'Intermediate', '1.7 km', '0m', 0, '0,42,43,33,44,45,46,40,47,41,999', 999)
+	mag3 = Course('mag', 'Magnuson Park', 3, 'Advanced', '2.8 km', '0m', 0, '0,36,37,38,39,40,35,41,43,32,999', 999)
+	fts1 = Course('fts', 'Ft. Steilacoom Park', 1, 'Beginner', '1.1 km', '15m', 0, '0,33,34,35,36,37,38,999', 999)
+	db.session.add(mag1)
+	db.session.add(mag2)
+	db.session.add(mag3)
+	db.session.add(fts1)
+	db.session.commit()
+	return 'done'
 
 
 ################################################
@@ -90,6 +98,7 @@ def query_db(query, args=(), one=False):
 def index():
 	'''page should display general information about what courses are avaiable'''
 	print "INDEX FUNCTION"
+	### CONVERT THIS DB CALL
 	locs = query_db('SELECT venuecode, venue_fullname FROM courses')
 	venues = []
 	for d in locs:
@@ -105,6 +114,7 @@ def venue(venuecode):
 	'''look up the code in the db, get the name + courses, display it
 	if the code is not valid, redirect to a landing page'''
 
+	### CONVERT THIS DB CALL
 	courses = query_db('SELECT venuecode, venue_fullname, course, course_name, distance FROM courses WHERE venuecode = ?', [venuecode])
 	if len(courses) == 0:
 		#should probably error here, but for now redirect
@@ -125,15 +135,18 @@ def init_course(venuecode, course):
 
 	#to-do VALIDATE that venue and course are valid values!!!
 
+	### CONVERT THIS DB CALL
 	sql  = 'INSERT INTO runners '
 	sql += '(venuecode, course, finished) '
 	sql += 'VALUES (?, ?, ?)'
 	sqlvals = [venuecode, course, 0]
 
+	### CONVERT THIS DB CALL
 	c = get_db().cursor()
 	c.execute(sql, sqlvals)
 	get_db().commit()
 
+	### CONVERT THIS DB CALL
 	session['runnerID'] = c.lastrowid
 
 	return redirect(url_for('visit_control',
@@ -153,7 +166,9 @@ def visit_control(venuecode, control):
 	if runnerid == None:
 		return redirect(url_for('venue', venuecode=venuecode))
 
+	### CONVERT THIS DB CALL
 	runner = query_db("SELECT * FROM runners WHERE id = ?", [runnerid], True)
+	### CONVERT THIS DB CALL
 	course = query_db("SELECT * FROM courses WHERE venuecode=? AND course=?",
 					  [runner['venuecode'], runner['course']], True)
 	controls = [int(a.strip(',')) for a in str(course['controls']).split()]
@@ -166,6 +181,7 @@ def visit_control(venuecode, control):
 	#evaluate control's validity for this runner
 	if (control == controls[0]) and (runner['punch_on_course'] == None):
 		#START case
+		### CONVERT THIS DB CALL
 		query_db('UPDATE runners SET punch=?, punch_on_course=? WHERE id=?',
 				[control, control, runner['id']])
 		get_db().commit()
@@ -175,6 +191,7 @@ def visit_control(venuecode, control):
 	elif (control == controls[-1]) and (controls.index(runner['punch_on_course']) == len(controls)-2):
 		#FINISH case
 		#to-do add a "finished" state to the db so re-loads don't mess up.
+		### CONVERT THIS DB CALL
 		query_db('UPDATE runners SET punch=?, punch_on_course=?, finished=? WHERE id=?',
 				[control, control, 1, runner['id']])
 		get_db().commit()
@@ -190,6 +207,7 @@ def visit_control(venuecode, control):
 
 	elif (controls.index(control)-1 == controls.index(runner['punch_on_course'])):
 		#on course case
+		### CONVERT THIS DB CALL
 		query_db('UPDATE runners SET punch=?, punch_on_course=? WHERE id=?',
 				[control, control, runner['id']])
 		get_db().commit()
@@ -198,6 +216,7 @@ def visit_control(venuecode, control):
 
 	else:
 		#incorrect case
+		### CONVERT THIS DB CALL
 		query_db('UPDATE runners SET punch=? WHERE id=?', [control, runner['id']])
 		get_db().commit()
 		nextcontrol = controls[controls.index(runner['punch_on_course'])+1]
@@ -220,6 +239,7 @@ def visit_control(venuecode, control):
 
 @app.route('/viewdb')
 def show_runners():
+	### CONVERT THIS DB CALL
 	runners = query_db('SELECT * FROM runners')
 	if len(runners) == 0:
 		return "oops, looks like the DB is empty."
